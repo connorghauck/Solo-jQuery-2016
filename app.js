fa79293c12e186d15d8d3d4e5b619a896513a875
Connor Hauck <connorghauck@yahoo.com>
2016-10-03 16:13:02 -0500

    Commit with changes to index and app which fit the criteria.

--- app.js
+++ app.js
@@ -1,34 +1,53 @@
 //must take the buttons and make them append boxes to the dom
 //you must append color-cube+.red, .yellow, .green, .blue.
-$(function(){})
+$(function(){
+    $(".color-button").click(function(){
+        var colour = $(this).data("color");
+        if ('red' == colour){
+            redBox();
+        } else if ('yellow' == colour){
+            yellowBox();
+        } else if ('green' == colour){
+            greenBox();
+        } else if ('blue' == colour){
+            blueBox();
+        }
+    });
+})
+
+
+
+var valueRed = 0;
+var valueYellow = 0;
+var valueGreen = 0;
+var valueBlue = 0;
+
+
+
 
 function redBox(){
 $('.container').append('<div class="color-cube red"></div>');
-var value = 0;
-value++
-$('#red').text('Total red: ' + value);
+valueRed++
+$('#red').text('Total red: ' + valueRed);
 }
-// HTMLElementObject.innerHTML=text
+
 
 function yellowBox(){
 $('.container').append('<div class="color-cube yellow"></div>');
-var value = 0;
-value++
-$('#yellow').text('Total yellow: ' + value);
+valueYellow++
+$('#yellow').text('Total yellow: ' + valueYellow);
 }
 
 
 function greenBox(){
 $('.container').append('<div class="color-cube green"></div>');
-var value = 0;
-value++
-$('#green').text('Total green: ' + value);
+valueGreen++
+$('#green').text('Total green: ' + valueGreen);
 }
 
 
 function blueBox(){
 $('.container').append('<div class="color-cube blue"></div>');
-var value = 0;
-value++
-$('#blue').text('Total blue: ' + value);
+valueBlue++
+$('#blue').text('Total blue: ' + valueBlue);
 }
